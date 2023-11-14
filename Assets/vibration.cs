d1eb8546591cd3895bfaafd81fb5a53f125b9142
bairemkh <bairemkh@gmail.com>
2023-11-14 20:49:32 +0100

    fixing vibrate

--- Assets/vibration.cs
+++ Assets/vibration.cs
@@ -1,59 +1,78 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
-public static class Vibration
+public class VibrationController : MonoBehaviour
 {
+    private AndroidJavaObject androidVibrator;
+    private Coroutine vibrationCoroutine;
 
-#if UNITY_ANDROID && !UNITY_EDITOR
-    public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-    public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-    public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
-#else
-    public static AndroidJavaClass unityPlayer;
-    public static AndroidJavaObject currentActivity;
-    public static AndroidJavaObject vibrator;
-#endif
-
-    public static void Vibrate()
+    void Start()
     {
-        if (isAndroid())
-            vibrator.Call("vibrate");
-        else
-            Handheld.Vibrate();
+        Initialize();
     }
 
-
-    public static void Vibrate(long milliseconds)
+    private void Initialize()
     {
+        // Check if the device is Android
         if (isAndroid())
-            vibrator.Call("vibrate", milliseconds);
-        else
-            Handheld.Vibrate();
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            androidVibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+        }
     }
 
-    public static void Vibrate(long[] pattern, int repeat)
+    public void StartVibration()
     {
         if (isAndroid())
-            vibrator.Call("vibrate", pattern, repeat);
+        {
+            // Start the vibration coroutine
+            vibrationCoroutine = StartCoroutine(LongVibrationCoroutine());
+        }
         else
+        {
+            // If not on Android, use default Unity vibration
             Handheld.Vibrate();
+        }
     }
 
-    public static bool HasVibrator()
+    public void StopVibration()
+    {
+        if (isAndroid() && vibrationCoroutine != null)
+        {
+            // Stop the vibration coroutine
+            StopCoroutine(vibrationCoroutine);
+            // Stop the vibration
+            androidVibrator.Call("cancel");
+        }
+        else
+        {
+            // If not on Android, or if vibration coroutine is not running, stop the default Unity vibration
+            Handheld.Vibrate();
+        }
+    }
+
+    private IEnumerator LongVibrationCoroutine()
+    {
+        // Vibrate for 10 seconds (adjust the duration as needed)
+        androidVibrator.Call("vibrate", 10000);
+
+        // Wait for the vibration duration to complete
+        yield return new WaitForSeconds(10.0f);
+
+        // Stop the vibration
+        androidVibrator.Call("cancel");
+    }
+
+    public bool HasVibrator()
     {
         return isAndroid();
     }
 
-    public static void Cancel()
-    {
-        if (isAndroid())
-            vibrator.Call("cancel");
-    }
-
-    private static bool isAndroid()
+    private bool isAndroid()
     {
 #if UNITY_ANDROID && !UNITY_EDITOR
-	return true;
+        return true;
 #else
         return false;
 #endif
